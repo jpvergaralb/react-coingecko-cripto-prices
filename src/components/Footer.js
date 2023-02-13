ae77821d14b2c73de75fce27f1e1d1c8a1fb02b1
jpvergaralb <jpvergaralb@uc.cl>
2023-02-13 19:44:05 -0300

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+    <footer className="p-3 bg-gh-softer-blue mt-10 rounded-t-sm shadow-lg flex items-center justify-center">
+    <span className="text-sm font-light text-slate-300 sm:text-center">© 2023 <a href="https://github.com/jpvergaralb" class="hover:underline">jpvergaralb™</a>. By using this you agree to give me your soul.
+    </span>
+
+</footer>
+  )
+}
+
+export default Footer
