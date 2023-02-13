cb78eca5860d794f6e11409a579a1c2062254944
jpvergaralb <jpvergaralb@uc.cl>
2023-02-13 07:33:42 -0300

    logic for each row

---
+++ src/components/CoinRow.js
@@ -0,0 +1,18 @@
+function CoinRow({ coin, index }) {
+  return (
+    <tr className="border border-slate-500 bg-gh-softer-blue">
+      <td className="text-center text-sm"> <code> {index} </code> </td>
+      <td className="flex justify-start">
+        {" "}
+        <img src={coin.image} alt={coin.name} width={24}  className="m-2 object-contain" />{" "}
+        <code className="ml-2 mt-2"> {coin.name} </code>{" "}
+      <h3 className="ml-2 mt-2.5 text-slate-500 font-extrabold uppercase text-sm"> {coin.symbol} </h3>{" "}
+      </td>
+      <td className="text-center "> <code> ${coin.current_price} </code></td>
+      <td className="text-center"> <code> ${coin.total_volume} </code></td>
+      <td className={`text-center ${coin.price_change_percentage_24h < 0 ? "text-red-500" : "text-green-400"}`}> <code> ${coin.price_change_percentage_24h}</code></td>
+    </tr>
+  );
+}
+
+export default CoinRow;
