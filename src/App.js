a1b510c08fdf86b3117536dcf1e1b2cb6c700e58
jpvergaralb <jpvergaralb@uc.cl>
2023-02-13 19:44:37 -0300

    add page navigation

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import TableCoins from "./components/TableCoins";
 import InputSearch from "./components/InputSearch";
+import PrevNext from "./components/PrevNext";
+import Footer from "./components/Footer";
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1)
 
   const getData = async () => {
     const response = await axios.get(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${currentPage}&sparkline=false`
     );
     const data = await response.data;
     setCoins(data);
@@ -18,13 +21,16 @@ function App() {
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [currentPage]);
 
   return (
     <div className="h-screen bg-gh-blue text-slate-200 overflow-y-auto">
       <Navbar />
       <InputSearch setSearch={setSearch}/>
+      <PrevNext quantity={coins.length} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
       <TableCoins coins={coins} search={search}/>
+      <PrevNext quantity={coins.length} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+      <Footer />
     </div>
   );
 }
