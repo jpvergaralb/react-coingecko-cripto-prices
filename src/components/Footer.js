ed01d5b5c57d071d9a7ec6b2396d65cc2c16c26a
jpvergaralb <jpvergaralb@uc.cl>
2023-02-14 01:19:24 -0300

    fix footer: now its really on the bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@
 
 function Footer() {
   return (
-    <footer className="p-3 bg-gh-softer-blue mt-10 rounded-t-sm shadow-lg flex items-center justify-center">
+    <footer className="p-3 bg-gh-softer-blue rounded-t-sm shadow-lg flex items-center justify-center mt-auto">
     <span className="text-sm font-light text-slate-300 sm:text-center">© 2023 <a href="https://github.com/jpvergaralb" class="hover:underline">jpvergaralb™</a>. By using this you agree to give me your soul.
     </span>
 
